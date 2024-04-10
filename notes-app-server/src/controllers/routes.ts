0bf3ab9f3a2bc0d83835998960059fa42f052fb8
MagicKriss <krisjanis.kallings@gmail.com>
2024-04-10 21:05:16 +0300

    feat: Setting up docker & docker-compose

--- notes-app-server/src/controllers/routes.ts
+++ notes-app-server/src/controllers/routes.ts
@@ -3,9 +3,9 @@ import { AppDependencies } from "../application/setup";
 import { createV1Routes } from "./v1/routes";
 
 export const createRoutes = (env: AppDependencies) => {
-    const router = express.Router();
-    const v1Routes = createV1Routes(env);
-    router.use("/api/v1", v1Routes);
-    // Add more versions here
-    return router;
+  const router = express.Router();
+  const v1Routes = createV1Routes(env);
+  router.use("/api/v1", v1Routes);
+  // Add more versions here
+  return router;
 };
